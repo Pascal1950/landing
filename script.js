f3521b2c0c08ede1443c897c996d1297ff48815b
Pascal1950 <128435852+Pascal1950@users.noreply.github.com>
2023-04-01 00:29:58 +0000

    cambio en boton acerca de

--- script.js
+++ script.js
@@ -9,4 +9,13 @@ var boton = document.getElementsByClassName("link")[0];
 boton.addEventListener("click", function() {
   saludo();
 });
+function despedir() {
+  alert("Gracias por visitarnos");
+  
+}
+var chao = document.getElementsByClassName("link")[1];
 
+chao.addEventListener("click", function() {
+ despedir();
+
+});
